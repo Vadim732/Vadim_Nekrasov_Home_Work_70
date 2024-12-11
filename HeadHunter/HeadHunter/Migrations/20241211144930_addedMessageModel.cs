835ffa8a92c3b8fd984155f973ba45ecddc44f00
Nekrasov Vadim <vadim65337.68@gmail.com>
2024-12-11 20:50:23 +0600

    Добавлена модель Message

---
+++ HeadHunter/HeadHunter/Migrations/20241211144930_addedMessageModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HeadHunter.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedMessageModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ HeadHunter/HeadHunter/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HeadHunter.Models;
+
+public class Message
+{
+    public int Id { get; set; }
+    [Required] public string Inscription { get; set; }
+    public DateTime DateOfDispatch { get; set; }
+    public int? UserId { get; set; }
+    public User? User { get; set; }
+    [Required] public int VacancyId { get; set; }
+    public Vacancy Vacancy { get; set; }
+    [Required] public int ResumeId { get; set; }
+    public Resume Resume { get; set; }
+}
